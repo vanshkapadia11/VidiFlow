633fabe19ff0b893ae465abde6b43341508f4b56
Vansh Kapadia <144871405+vanshkapadia11@users.noreply.github.com>
2026-03-10 00:50:11 +0530

    Added The Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono, JetBrains_Mono } from "next/font/google";
 import "./globals.css";
 import { Providers } from "./providers";
+import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 // import { Providers } from "./providers";
 
 const jetbrainsMono = JetBrains_Mono({subsets:['latin'],variable:'--font-sans'});
